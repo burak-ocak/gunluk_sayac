81b073880f3c65ecc59743fe86bad89779067dc0
Burak Ocak <ocakbura@gmail.com>
2023-02-18 22:11:35 +0300

    feat: saat, dakika ve saniye arttırması yapıldı.

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
+let sayacBasladiMi = false;
+
 let timeText = document.querySelector("#timeText");
-let saat = timeText.querySelector("#saat");
-let dakika = timeText.querySelector("#dakika");
-let saniye = timeText.querySelector("#saniye");
+let hour = timeText.querySelector("#hour");
+let minute = timeText.querySelector("#minute");
+let second = timeText.querySelector("#second");
 let start = document.querySelector("#start");
+
 let stop = document.querySelector("#stop");
 let finish = document.querySelector("#finish");
 
-let bir = 0;
-let iki = 0;
-let uc = 0;
-    
-function test(){
-    bir++;
 
-    if(bir < 60){
-        saniye.textContent = bir + "sn";
+let sec = 0;
+let min = 0;
+let hours = 0;
+    
+
+start.addEventListener("click", function(){
+    setInterval(test, 1000);
+});
+
+
+
+function test(){
+    sec++;
+
+    if(sec < 60){
+        second.textContent = sec + " saniye";
     }
 
-}
+    if(sec > 60){
+        sec = 0;
+        second.textContent = sec + " saniye";
+        min++;
+        minute.textContent = min + " dakika";
+    }
+    
+    if(min > 60){
+        min = 0;
+        minute.textContent = min + " dakika";
+        hours++;
+        hour.textContent = hours + " saat";
+    }
+};
 
-setInterval(test, 1000);
+
+
